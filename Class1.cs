e3d0daf31ab7f04390871251c45c600b5b362c73
andryxatt <vlandroid228@gmail.com>
2024-05-19 11:54:52 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace ConsoleApp26
+{
+    public class Film : IDisposable
+    {
+        public string Name { get; set; }
+        public string NameStudio { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public int Year { get; set; }
+        public Film(string n, string ns, string g, int d, int y)
+        {
+            Name = n;
+            NameStudio = ns;
+            Genre = g;
+            Duration = d;
+            Year = y;
+        }
+        ~Film()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            //Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}\nNameStudio: {NameStudio}\n" +
+                $"Genre: {Genre}\nDuration: {Duration}\nYear: {Year}");
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp26
+{
+    internal class Program
+    {
+      static void Main(string[] args)
+      {
+        Film film = new Film("Test1", "Test2", "Test3", 2, 4);
+        film.Show();
+      }
+    }
+}
